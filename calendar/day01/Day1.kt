8fc84ee075f5e01701d26ab65e386be67e4f3015
mmirk <mmirk@expediagroup.com>
2022-12-02 16:07:11 +0100

    day 1 solution (procedural)

--- calendar/day01/Day1.kt
+++ calendar/day01/Day1.kt
@@ -5,10 +5,27 @@ import Lines
 
 class Day1 : Day() {
     override fun part1(input: Lines): Any {
-        TODO("Not yet solved")
+        return input.mostCalories(1).first()
     }
 
     override fun part2(input: Lines): Any {
-        TODO("Not yet solved")
+        return input.mostCalories(3).sum()
+    }
+
+    private fun Lines.mostCalories(take: Int): List<Int> {
+        val mostCalories = sortedSetOf<Int>()
+        var totalCalories = 0
+        forEach { line ->
+            if (line.isNotEmpty()) {
+                totalCalories += line.toInt()
+            } else {
+                mostCalories += totalCalories
+                totalCalories = 0
+            }
+        }
+        return mostCalories.descendingIterator()
+            .asSequence()
+            .take(take)
+            .toList()
     }
 }
