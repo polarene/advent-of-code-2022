13705b31ebcebdaf35a3fba907ec6f7fb31ff0ba
mmirk <mmirk@expediagroup.com>
2022-12-05 11:05:44 +0100

    day 4 solution (oop/fp)

--- calendar/day04/Day4.kt
+++ calendar/day04/Day4.kt
@@ -5,10 +5,41 @@ import Lines
 
 class Day4 : Day() {
     override fun part1(input: Lines): Any {
-        TODO("Not yet solved")
+        return input.map(::parseAssignments)
+            .count { it.fullyOverlaps() }
     }
 
     override fun part2(input: Lines): Any {
-        TODO("Not yet solved")
+        return input.map(::parseAssignments)
+            .count { it.overlaps() }
     }
+}
+
+fun parseAssignments(line: String): ElvesPair {
+    return line.split(",")
+        .map(::getSections)
+        .let { ElvesPair(it[0], it[1]) }
+}
+
+private fun getSections(sectionIdsRange: String) = sectionIdsRange.split("-")
+    .let {
+        val (start, end) = it
+        start.toInt()..end.toInt()
+    }
+
+class ElvesPair(private val assignment1: IntRange, private val assignment2: IntRange) {
+    fun fullyOverlaps(): Boolean {
+        return assignment1 fullyContains assignment2
+                || assignment2 fullyContains assignment1
+    }
+
+    fun overlaps(): Boolean {
+        return assignment2.first in assignment1
+                || assignment2.last in assignment1
+                || assignment1.first in assignment2
+                || assignment1.last in assignment2
+    }
+
+    private infix fun IntRange.fullyContains(other: IntRange) =
+        first <= other.first && last >= other.last
 }
