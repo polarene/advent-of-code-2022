c13036c8326ea1d0182965e6fbcd74cb9a1ad3b3
mmirk <mmirk@expediagroup.com>
2022-12-06 13:00:55 +0100

    refactored Day 1 implementing chunkedWhile()

--- calendar/day01/Day1.kt
+++ calendar/day01/Day1.kt
@@ -5,27 +5,30 @@ import Lines
 
 class Day1 : Day() {
     override fun part1(input: Lines): Any {
-        return input.mostCalories(1).first()
+        return input.mostCalories(1).single()
     }
 
     override fun part2(input: Lines): Any {
         return input.mostCalories(3).sum()
     }
 
-    private fun Lines.mostCalories(take: Int): List<Int> {
-        val mostCalories = sortedSetOf<Int>()
-        var totalCalories = 0
-        forEach { line ->
-            if (line.isNotEmpty()) {
-                totalCalories += line.toInt()
-            } else {
-                mostCalories += totalCalories
-                totalCalories = 0
-            }
+    private fun Lines.mostCalories(take: Int) =
+        chunkedWhile { it.isNotEmpty() }
+            .map { it.map(String::toInt).sum() }
+            .sorted()
+            .takeLast(take)
+}
+
+fun <T> Iterable<T>.chunkedWhile(predicate: (T) -> Boolean): List<List<T>> {
+    val result = mutableListOf<List<T>>()
+    var chunk = mutableListOf<T>()
+    for (element in this) {
+        if (predicate(element)) {
+            chunk += element
+        } else {
+            result += chunk
+            chunk = mutableListOf()
         }
-        return mostCalories.descendingIterator()
-            .asSequence()
-            .take(take)
-            .toList()
     }
+    return result
 }
